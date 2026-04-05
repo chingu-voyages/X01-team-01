e70ee02a9f6c7687366acb968130e51685948fe9
ivannissimrch <ivannissimrch@gmail.com>
2026-04-05 16:14:53 -0400

    Add basic layout, header, footer 2 test pages and a not found page

---
+++ Frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-background relative overflow-hidden flex justify-center border-t border-border p-4 w-full">
+      <div className="container">
+        <div className="flex justify-between">
+          <p className="">Footer</p>
+          <div>links</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
