9aacb9cd27c28009148c03dbe74f20f421004acd
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-04-17 09:34:41 +0200

    Update Footer.tsx

--- Frontend/src/components/layout/Footer.tsx
+++ Frontend/src/components/layout/Footer.tsx
@@ -1,10 +1,67 @@
+import Image from "next/image";
+import ChinguLogo from "@/media/chingu-logo.png"
+
+const dateFormatter = Intl.DateTimeFormat("en-US", {dateStyle: "long"})
+
 export default function Footer() {
+  const today: Date = new Date()
+  const formattedDate: string = dateFormatter.format(today)
+
   return (
     <footer className="bg-background relative overflow-hidden flex justify-center border-t border-border p-4 w-full">
       <div className="container">
-        <div className="flex justify-between">
-          <p className="">Footer</p>
-          <div>links</div>
+        <div className="flex justify-between items-center">
+          <p className="">
+            <Image src={ChinguLogo} alt="Chingu logo" className="w-10"/>
+          </p>
+          <ul className="flex justify-between gap-4">
+            <li className="border-r border-r-black/30 pr-4">
+              <a
+                href="https://www.linkedin.com/in/lillatoth216/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Lilla Tóth
+              </a>
+            </li>
+            <li className="border-r border-r-black/30 pr-4">
+              <a
+                href="https://www.linkedin.com/in/ivan-rebolledo-012b17244/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Ivan Rebolledo
+              </a>
+            </li>
+            <li className="border-r border-r-black/30 pr-4">
+              <a
+                href="https://www.linkedin.com/in/omar-ramos-correa-7621253b2/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Omar Ramos-Correa
+              </a>
+            </li>
+            <li className="border-r border-r-black/30 pr-4">
+              <a
+                href="https://www.linkedin.com/in/yangchendema/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Yangchen Dema
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/chinedu-olekah"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Chinedu Olekah
+              </a>
+            </li>
+          </ul>
+          <p>{formattedDate}</p>
         </div>
       </div>
     </footer>
