ddc5e901a9da2f2a5b6afaf1f44e8ce098646b55
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-05-15 17:23:05 +0200

    feat: implement useHistory hook for paginated list management

---
+++ Frontend/src/hooks/useHistory.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { Prompt } from "@/types/history";
+import fetchMockHistory from "@/app/utils/historyHelpers";
+
+export const useHistory = (initialData: Prompt[]) => {
+  const [visiblePrompts, setVisiblePrompts] = useState<Prompt[]>([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  //initial load
+  useEffect(() => {
+    const { data, hasMore: moreAvaliable } = fetchMockHistory(initialData, 0);
+    setVisiblePrompts(data);
+    (setOffset(3), setHasMore(moreAvaliable));
+  }, [initialData]);
+
+  function loadMore() {
+    const { data, hasMore: moreAvaliable } = fetchMockHistory(
+      initialData,
+      offset,
+    );
+
+    setVisiblePrompts((prev) => [...prev, ...data]);
+    setOffset((prev) => prev + 3);
+    setHasMore(moreAvaliable);
+  }
+
+  return { visiblePrompts, loadMore, hasMore };
+};
