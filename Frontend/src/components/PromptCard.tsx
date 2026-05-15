f7a7037c3d23a6397c72ea2d85dfedbeafd4f78d
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-05-15 17:41:01 +0200

    feat: add onClick prop to PromptCard for detail view trigger

--- Frontend/src/components/PromptCard.tsx
+++ Frontend/src/components/PromptCard.tsx
@@ -2,11 +2,15 @@ import { Prompt } from "@/types/history";
 
 interface PromptCardProps {
   data: Prompt;
+  onClick: (data: Prompt) => void;
 }
 
-export default function PromptCard({ data }: PromptCardProps) {
+export default function PromptCard({ data, onClick }: PromptCardProps) {
   return (
-    <article className="bg-gray-200 rounded-md p-4">
+    <article
+      className="bg-gray-200 rounded-md p-4 cursor-pointer hover:bg-gray-300 transition-colors"
+      onClick={() => onClick(data)}
+    >
       <div className="text-xs pb-4">{data.date}</div>
       <div className="text-base pb-2 font-semibold">{data.task}</div>
       <div className="text-sm line-clamp-3">{data.prompt}</div>
