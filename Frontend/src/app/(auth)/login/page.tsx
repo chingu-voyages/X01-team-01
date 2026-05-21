693a9c5cfc979b7db00c585152e2d440bea09b8a
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-05-21 18:06:57 +0200

    ui: temporary UI updates

--- Frontend/src/app/(auth)/login/page.tsx
+++ Frontend/src/app/(auth)/login/page.tsx
@@ -13,15 +13,18 @@ export default function LoginPage() {
     <div className="max-h-[90vh] md:min-h-screen w-full grid place-items-center bg-slate-50">
       <div className="w-[90%] sm:max-w-md p-6 sm:p-8 bg-white rounded-xl shadow-md">
         <div className="flex flex-col items-center mb-8">
-          <h1 className="tracking-tighter font-normal text-2xl sm:text-3xl">
-            AI Helper
+          <h1 className="tracking-tight text-3xl sm:text-5xl font-black text-gray-900 leading-none">
+            AI{" "}
+            <span className="text-primary bg-linear-to-r from-primary to-primary/70 bg-clip-text">
+              Helper
+            </span>
           </h1>
-          <h3 className="tracking-tighter font-light text-xl sm:text-2xl text-black/70">
+          <h3 className="tracking-tight font-normal text-base sm:text-xl text-gray-500 max-w-xs mx-auto">
             Build better prompts.
           </h3>
         </div>
         <div className="flex flex-col gap-3 w-full">
-          <p className="tracking-tight text-center text-sm sm:text-base text-black/70">
+          <p className="tracking-tight text-center text-sm sm:text-base text-gray-500">
             Sign in with:
           </p>
           <Button variant="default" className="h-10">
@@ -29,7 +32,7 @@ export default function LoginPage() {
           </Button>
           <div className="flex items-center w-full my-2">
             <div className="grow border-t border-slate-300" />
-            <span className="mx-4 text-sm font-medium text-slate-500 tracking-wider">
+            <span className="mx-4 text-sm font-medium text-gray-500 tracking-wider">
               or
             </span>
             <div className="grow border-t border-slate-300" />
@@ -42,10 +45,10 @@ export default function LoginPage() {
           <button
             type="button"
             onClick={() => {
-              dispatch(setGuestMode())
-              router.push("/home")
+              dispatch(setGuestMode());
+              router.push("/home");
             }}
-            className="text-sm text-slate-600 hover:text-slate-900 underline underline-offset-3 font-medium transition-colors mt-4 block mx-auto"
+            className="text-sm text-gray-500 hover:text-slate-900 underline underline-offset-3 font-medium transition-colors mt-4 block mx-auto"
           >
             Continue without signing in.
           </button>
